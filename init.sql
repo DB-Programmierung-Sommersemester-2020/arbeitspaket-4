be25cef9d22055991b7addcd1e2fd7fbf43b8b98
VChouliak <chouliak@hotmail.com>
2020-05-20 14:20:24 +0200

    Initial commit

---
+++ init.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS usernotes;
+
+CREATE DATABASE usernotes CHARACTER SET utf8mb4;
+
+USE usernotes;
+
+CREATE TABLE Users (id INT PRIMARY KEY AUTO_INCREMENT,
+                    username VARCHAR(50),
+                    password VARCHAR(50),
+                    email VARCHAR(50) );
+
+CREATE TABLE Notes (id INT PRIMARY KEY AUTO_INCREMENT,
+                    subject VARCHAR(100),
+                    content VARCHAR(1024),
+                    date VARCHAR(100) ,
+                    userId INT);
