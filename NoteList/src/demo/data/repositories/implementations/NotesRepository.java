266978d076c2d929a990b471575a17d6da7f8e45
VChouliak <chouliak@hotmail.com>
2020-05-22 00:54:00 +0200

    Implement services

---
+++ NoteList/src/demo/data/repositories/implementations/NotesRepository.java
@@ -0,0 +1,7 @@
+package demo.data.repositories.implementations;
+
+import demo.data.entities.Note;
+
+public class NotesRepository extends AbstractCRUDRepository<Note, Integer>{
+
+}
---
+++ NoteList/src/demo/data/repositories/implementations/UsersRepository.java
@@ -0,0 +1,7 @@
+package demo.data.repositories.implementations;
+
+import demo.data.entities.User;
+
+public class UsersRepository extends AbstractCRUDRepository<User, Integer> {
+
+}
